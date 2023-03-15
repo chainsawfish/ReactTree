0f26683f28a8fae71cfa453ad897ae402d17918b
chainsiber <chainsawfish@siberteam.com>
2023-03-15 08:48:48 +0000

    Update App.jsx

--- react-tree/src/App.jsx
+++ react-tree/src/App.jsx
@@ -1,35 +1,33 @@
 import "react";
-import {useContext, createContext} from "react";
+import { createContext } from "react";
 import Node from "./components/Node";
 import { nodeList } from "./data/nodeList.js";
 import NavBar from "./components/NavBar.jsx";
-import './App.css'
+import "./App.css";
 
-export const TreeContext = createContext(null)
+export const TreeContext = createContext(null);
 
 const handlers = {
-    handleAdd: () => {},
-    handleRemove: () => {},
-    handleEdit: () => {},
-    handleReset: () => {},
-}
+  handleAdd: () => {},
+  handleRemove: () => {},
+  handleEdit: () => {},
+  handleReset: () => {},
+};
 
 function App() {
-
-    const contextValue = {
-        selectedId: 0,
-        ...handlers
-    }
+  const contextValue = {
+    selectedId: 0,
+    ...handlers,
+  };
 
   return (
     <div className="App">
-        <TreeContext.Provider value={contextValue}>
-        <NavBar/>
+      <TreeContext.Provider value={contextValue}>
+        <NavBar />
         <Node nodes={nodeList} />
-        </TreeContext.Provider>
-
+      </TreeContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

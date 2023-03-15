d9448b36cc5a908eb71538f534daf1a9935d0781
chainsiber <chainsawfish@siberteam.com>
2023-03-15 09:19:15 +0000

    feat select nodes works now

--- react-tree/src/components/Node.jsx
+++ react-tree/src/components/Node.jsx
@@ -1,30 +1,32 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const Node = ({nodes}) => {
-    const [isVisible, setIsVisible] = useState(false);
+const Node = ({ nodes }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+  const toggleOpen = () => {
+    setIsVisible(!isVisible);
+  };
 
-    const toggleOpen = () => {
-        setIsVisible(!isVisible);
-    };
+  const selectedNode = (node) => {
+    console.log("selected node", node);
+  };
 
-    const toggleSelect = () => {
-
-    }
-
-    return (
-        <div style={{paddingLeft: 30}}>
-
-            <span onClick={toggleOpen}>{isVisible ? "- " : "+ "}</span>
-            <span onClick={toggleSelect}>Node {nodes?.id}</span>
-            {isVisible && (
-                nodes?.children?.map((childNode) => {
-                    return (
-                        <Node key={childNode.id} nodes={childNode}/>
-                    );
-                })
-            )}
-        </div>
-    );
-}
+  return (
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        selectedNode(nodes?.id);
+      }}
+      style={{ paddingLeft: 30 }}
+    >
+      <span onClick={toggleOpen}>{isVisible ? "- " : "+ "}</span>
+      <span>Node {nodes?.id}</span>
+      {isVisible &&
+        nodes?.children?.map((childNode) => {
+          return <Node key={childNode.id} nodes={childNode} />;
+        })}
+    </div>
+  );
+};
 
 export default Node;
